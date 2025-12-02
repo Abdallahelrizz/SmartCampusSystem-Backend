0c2627f55bad74f21007bac4c07d1280c6d93dbc
Assil Halawi <assilhalawi1@gmail.com>
2025-12-02 22:53:34 +0200

    Testing Cursor changes

--- routes/chatbotRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const ChatbotController = require('../controllers/chatbotController');
-const { authenticateToken } = require('../middleware/auth');
-
-router.post('/chat', authenticateToken, ChatbotController.getResponse);
-
-module.exports = router;
-
